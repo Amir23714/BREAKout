774a072deccdafaa7dda313ceea2e79bff3a51e0
Amir23714 <mcclrn_bk@mail.ru>
2023-06-18 15:07:10 +0300

    dsadsdsa

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,13 +1,22 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardRemove, \
     ReplyKeyboardMarkup, KeyboardButton, \
     InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+data = MemoryStorage()
+
+class LogInStates(StatesGroup):
+    api_key = State()
+    secret_key = State()
+    logged_in = State()
 
 bot = Bot(token='6234279060:AAFx1KgWvVNg1prHpQfvlS203nZaOt4IH5U')
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 ## Keyboard
 button_authorization = KeyboardButton('Log in 🤳')
@@ -16,6 +25,16 @@ button_statistics = KeyboardButton('Statistics 💻')
 
 first_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_client_work).add(button_statistics)
 first_kb_no_admins = ReplyKeyboardMarkup(resize_keyboard=True).add(button_authorization)
+
+def get_debug_kb():
+
+    button_back = KeyboardButton('Back 🔙')
+    button_back_to_main = KeyboardButton('Back to main menu 🔙')
+
+    debug_kb = ReplyKeyboardMarkup(resize_keyboard=True).add(button_back).add(button_back_to_main)
+
+    return debug_kb
+
 
 ## Admins
 admins = [682751445, 1992272849]
@@ -29,17 +48,70 @@ async def alarm(message: types.Message):
         await message.answer(f"Greetings, {message.from_user.username}, log in!", reply_markup=first_kb_no_admins)
 
 
+
+@dp.message_handler(state = LogInStates.api_key)
+async def return_from_api_state(message: types.Message, state: FSMContext):
+    if message.text == 'Back 🔙':
+        await message.reply("Main menu", reply_markup=first_kb)
+        await state.finish()
+
+    elif message.text == 'Back to main menu 🔙':
+        await message.reply("Main menu", reply_markup=first_kb)
+        await state.finish()
+
+    else:
+        async with state.proxy() as data:
+            data['api_key'] = message.text
+
+        await message.reply("Enter SECRET_KEY which is tied to the work account:", reply_markup=get_debug_kb())
+        await LogInStates.secret_key.set()
+
+
+@dp.message_handler(state = LogInStates.secret_key)
+async def return_from_secret_state(message: types.Message, state: FSMContext):
+    if message.text == 'Back 🔙':
+        await message.reply("Add account section.\n"
+                        "\n"
+                        "Enter API_KEY which is tied to the work account:", reply_markup=get_debug_kb())
+        await LogInStates.api_key.set()
+
+    elif message.text == 'Back to main menu 🔙':
+        await message.reply("Main menu", reply_markup=first_kb)
+        await state.finish()
+
+    else:
+        async with state.proxy() as data:
+            data['secret_key'] = message.text
+
+        await message.reply(f"Your API_KEY: {data['api_key']}\nYour SECRET_KEY: {data['secret_key']}", reply_markup=get_debug_kb())
+        await LogInStates.logged_in.set()
+
+
+@dp.message_handler(state = LogInStates.logged_in)
+async def return_from_loggedin_state(message: types.Message, state: FSMContext):
+    if message.text == 'Back 🔙':
+        await message.reply("Enter SECRET_KEY which is tied to the work account:", reply_markup=get_debug_kb())
+        await LogInStates.secret_key.set()
+
+    elif message.text == 'Back to main menu 🔙':
+        await message.reply("Main menu", reply_markup=first_kb)
+        await state.finish()
+
+
+
 @dp.message_handler(text=['Add an account to work 🚜'])
 async def process_client_work_command(message: types.Message):
-    await message.reply(f"Add account section.\n"
-                        f"\n"
-                        f"Enter API_KEY and SECRET_KEY with a space, which is tied to the work account:")
+    await message.reply("Add account section.\n"
+                        "\n"
+                        "Enter API_KEY which is tied to the work account:", reply_markup=get_debug_kb())
+    await LogInStates.api_key.set()
+
+
 
 
 @dp.message_handler()
-async def get_phone(message: types.Message):
-    phone = message.text
-    await message.reply(f"Your API_KEY: {message.text.split()[0]}\nSECRET_KEY: {message.text.split()[1]}\n")
+async def echo(message: types.Message):
+    await message.answer("I don't understand you, try again", reply_markup=first_kb)
 
 
 if __name__ == '__main__':
